0ea3e1a28d13f8dbf865c0ccaa8cc2281df05f06
Laurențiu Păncescu <lpancescu@gmail.com>
2016-08-24 17:09:05 +0200

    Bug fix: 'vagrant up' takes a long time in rsync
    Since the directory containing the Vagrantfile is synced by default, we
    have to avoid placing the Vagrantfile directly in the root directory.
    Vagrant downloads the images in ~/vagrant.d, so we'll end up needlessly
    rsyncing hundreds of MB.

--- run_test.sh
+++ run_test.sh
@@ -4,6 +4,7 @@ set -eux
 scl enable sclo-vagrant1 'sh vagrant_add.sh'
 scl enable sclo-vagrant1 'vagrant box list'
 
+cd box
 scl enable sclo-vagrant1 'vagrant up c6'
 scl enable sclo-vagrant1 'vagrant ssh c6 -c "uname -a"'
 scl enable sclo-vagrant1 'vagrant destroy c6 -f'
