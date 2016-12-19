48bab738a15df022b89e5dfb7d9e6371d398ee5a
Laurențiu Păncescu <lpancescu@gmail.com>
2016-12-19 15:22:58 +0100

    sha256sum doesn't support --ignore-missing

--- install_vagrant.sh
+++ install_vagrant.sh
@@ -4,8 +4,7 @@ set -eux
 yum -y install centos-release-scl
 yum -y install sclo-vagrant1 qemu-kvm rsync
 curl -O http://download.virtualbox.org/virtualbox/5.0.30/VirtualBox-5.0-5.0.30_112061_el7-1.x86_64.rpm
-curl -O https://www.virtualbox.org/download/hashes/5.0.30/SHA256SUMS
-sha256sum --ignore-missing --check SHA256SUMS
+curl https://www.virtualbox.org/download/hashes/5.0.30/SHA256SUMS | grep 'el7-1\.x86_64\.rpm$' | sha256sum -c -
 yum -y install VirtualBox-5.0-5.0.30_112061_el7-1.x86_64.rpm
 yum -y install gcc make kernel-devel-$(uname -r)
 systemctl start libvirtd.service
