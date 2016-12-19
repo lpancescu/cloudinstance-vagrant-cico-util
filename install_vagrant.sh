03931ab008500b66db889737dce50bb5376beb55
Laurențiu Păncescu <lpancescu@gmail.com>
2016-12-19 11:40:48 +0100

    Use VirtualBox 5.0.30 for testing

--- install_vagrant.sh
+++ install_vagrant.sh
@@ -3,6 +3,9 @@ set -eux
 
 yum -y install centos-release-scl
 yum -y install sclo-vagrant1 qemu-kvm rsync
-yum -y install http://download.virtualbox.org/virtualbox/5.0.26/VirtualBox-5.0-5.0.26_108824_el7-1.x86_64.rpm
+curl -O http://download.virtualbox.org/virtualbox/5.0.30/VirtualBox-5.0-5.0.30_112061_el7-1.x86_64.rpm
+curl -O https://www.virtualbox.org/download/hashes/5.0.30/SHA256SUMS
+sha256sum --ignore-missing --check SHA256SUMS
+yum -y install VirtualBox-5.0-5.0.30_112061_el7-1.x86_64.rpm
 yum -y install gcc make kernel-devel-$(uname -r)
 systemctl start libvirtd.service
