d474f03708b103f95a55e6d1f830be030da70e5c
Laurențiu Păncescu <lpancescu@gmail.com>
2016-12-21 13:34:53 +0100

    Only install vagrant-vbguest when explicitly asked
    Using vagrant-vbguest adds around 10 minutes to the test duration, and
    also opens of possibility of test failures due to bugs in the VirtualBox
    Guest Additions (which will also taint the box kernel).
    
    You can add an --with-vagrant-vbguest argument to install_vagrant.sh in
    the cico configuration (cloudinstance-vagrant-test on ci.centos.org).

--- install_vagrant.sh
+++ install_vagrant.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
 set -eux
 
+# accept an optional argument --with-vagrant-vbguest
+while [ $# -gt 0 ]; do
+	arg="$1"
+	case $arg in
+		--with-vagrant-vbguest)
+			INSTALL_VBGUEST=1
+			;;
+		*)
+			echo "$(basename \"$0\"): invalid argument"
+			exit 1
+			;;
+	esac
+	shift
+done
+
 yum -y install centos-release-scl
 yum -y install sclo-vagrant1 qemu-kvm rsync
 curl -O http://download.virtualbox.org/virtualbox/5.0.30/VirtualBox-5.0-5.0.30_112061_el7-1.x86_64.rpm
 curl https://www.virtualbox.org/download/hashes/5.0.30/SHA256SUMS | grep 'el7-1\.x86_64\.rpm$' | sha256sum -c -
 yum -y install VirtualBox-5.0-5.0.30_112061_el7-1.x86_64.rpm
-yum -y install gcc make kernel-devel-$(uname -r)
-yum -y install rh-ruby22-ruby-devel gcc-c++ zlib-devel libvirt-devel
-scl enable sclo-vagrant1 'vagrant plugin install vagrant-vbguest'
+if [ ${INSTALL_VBGUEST:-0} -eq 1 ]; then
+	yum -y install rh-ruby22-ruby-devel gcc-c++ zlib-devel libvirt-devel
+	scl enable sclo-vagrant1 'vagrant plugin install vagrant-vbguest'
+fi
 systemctl start libvirtd.service
