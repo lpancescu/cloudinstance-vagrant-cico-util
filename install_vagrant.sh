8d60dd388006085474204cf7be338fe00c65f829
Laurențiu Păncescu <lpancescu@gmail.com>
2016-12-23 01:05:24 +0100

    VirtualBox needs to compile its kernel modules

--- install_vagrant.sh
+++ install_vagrant.sh
@@ -18,6 +18,8 @@ done
 
 yum -y install centos-release-scl
 yum -y install sclo-vagrant1 qemu-kvm rsync
+# VirtualBox needs to compile its kernel modules
+yum -y install gcc make kernel-devel-$(uname -r)
 curl -O http://download.virtualbox.org/virtualbox/5.0.30/VirtualBox-5.0-5.0.30_112061_el7-1.x86_64.rpm
 curl https://www.virtualbox.org/download/hashes/5.0.30/SHA256SUMS | grep 'el7-1\.x86_64\.rpm$' | sha256sum -c -
 yum -y install VirtualBox-5.0-5.0.30_112061_el7-1.x86_64.rpm
