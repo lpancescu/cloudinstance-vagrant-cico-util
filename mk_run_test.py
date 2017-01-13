1ae6e29df7ad9204b4b25b105891fb24da709c19
Laurențiu Păncescu <lpancescu@gmail.com>
2017-01-13 13:29:32 +0100

    Test with all supported VirtualBox versions

--- mk_run_test.py
+++ mk_run_test.py
@@ -3,6 +3,7 @@ from __future__ import print_function, unicode_literals
 
 import json
 import os
+import sys
 
 
 def test_provider(images, provider):
@@ -15,9 +16,11 @@ scl enable sclo-vagrant1 'vagrant ssh c{0} -c "uname -a"'
 scl enable sclo-vagrant1 'vagrant destroy c{0} -f'
 """.format(r, provider))
 
-images = json.loads(os.environ['BUILD_INFO'])
 
-print("""
+if __name__ == '__main__':
+    images = json.loads(os.environ['BUILD_INFO'])
+
+    print("""
 #!/bin/sh
 set -eux
 
@@ -26,13 +29,36 @@ scl enable sclo-vagrant1 'sh vagrant_add.sh'
 cd box
 """)
 
-test_provider(images, 'libvirt')
+    test_provider(images, 'libvirt')
 
-print("""
+    print("""
 # VirtualBox doesn't support nested virtualization
 systemctl stop libvirtd.service
 modprobe -r -v kvm_intel kvm_amd kvm
+
+# VirtualBox needs to compile its kernel modules
+yum -y install gcc make kernel-devel-$(uname -r)
+curl -O http://download.virtualbox.org/virtualbox/5.0.30/VirtualBox-5.0-5.0.30_112061_el7-1.x86_64.rpm
+curl https://www.virtualbox.org/download/hashes/5.0.30/SHA256SUMS | grep 'el7-1\.x86_64\.rpm$' | sha256sum -c -
+yum -y install VirtualBox-5.0-5.0.30_112061_el7-1.x86_64.rpm
+
 /sbin/rcvboxdrv setup # build and load the VirtualBox kernel modules
 """)
 
-test_provider(images, 'virtualbox')
+    if sys.argv == 2 and sys.argv[1] == '--with-vagrant-vbguest':
+        print("""
+yum -y install rh-ruby22-ruby-devel gcc-c++ zlib-devel libvirt-devel
+scl enable sclo-vagrant1 'vagrant plugin install vagrant-vbguest'
+""")
+
+    test_provider(images, 'virtualbox')
+
+    print("""
+yum -y remove VirtualBox-5.0
+curl -O http://download.virtualbox.org/virtualbox/5.1.12/VirtualBox-5.1-5.1.12_112440_el7-1.x86_64.rpm
+curl https://www.virtualbox.org/download/hashes/5.1.12/SHA256SUMS | grep 'el7-1\.x86_64\.rpm$' | sha256sum -c -
+yum -y install VirtualBox-5.1-5.1.12_112440_el7-1.x86_64.rpm
+/sbin/rcvboxdrv setup # build and load the VirtualBox kernel modules
+""")
+
+    test_provider(images, 'virtualbox')
