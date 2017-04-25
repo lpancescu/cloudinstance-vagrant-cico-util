0d4d328306aaaca580e96b5e4303c52111554d78
Laurențiu Păncescu <laurentiu@laurentiupancescu.com>
2017-04-25 16:42:53 +0200

    Install libvirt explicitly

--- install_vagrant.sh
+++ install_vagrant.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-yum -y install qemu-kvm rsync
+yum -y install libvirt qemu-kvm rsync
 yum -y install https://releases.hashicorp.com/vagrant/1.9.4/vagrant_1.9.4_x86_64.rpm
 systemctl start libvirtd.service
