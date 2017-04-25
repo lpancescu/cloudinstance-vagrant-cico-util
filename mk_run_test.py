add2236f9d3ad4b6e1c604b000e05f46afba89b7
Laurențiu Păncescu <laurentiu@laurentiupancescu.com>
2017-04-25 10:16:30 +0200

    Fix Python indentation to only use spaces

--- mk_run_test.py
+++ mk_run_test.py
@@ -10,13 +10,13 @@ from jinja2 import Environment, FileSystemLoader
 
 if __name__ == '__main__':
     images = json.loads(os.environ['BUILD_INFO'])
-    with_vbguest = len(sys.argv) == 2 \
-		       and sys.argv[1] == '--with-vagrant-vbguest'
+    with_vbguest = (len(sys.argv) == 2 and
+                    sys.argv[1] == '--with-vagrant-vbguest')
 
 
     env = Environment(loader=FileSystemLoader('templates'))
     # the 'equalto' filter was introduced in jinja 2.8
     if 'equalto' not in env.tests:
-	env.tests['equalto'] = lambda value, other : value == other
+        env.tests['equalto'] = lambda value, other : value == other
     template = env.get_template('run_test.j2')
     print(template.render(images=images, with_vbguest=with_vbguest))
