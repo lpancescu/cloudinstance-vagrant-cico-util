e2d8050c1c304c22b77d6e101eb1287b46838af9
Laurențiu Păncescu <laurentiu@laurentiupancescu.com>
2019-03-23 17:18:31 +0100

    Use Vagrant 2.2.4 and VirtualBox 6.0.4

--- install_vagrant.sh
+++ install_vagrant.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 yum -y install qemu libvirt libvirt-devel ruby-devel gcc qemu-kvm rsync
-yum -y install https://releases.hashicorp.com/vagrant/1.9.3/vagrant_1.9.3_x86_64.rpm
+yum -y install https://releases.hashicorp.com/vagrant/2.2.4/vagrant_2.2.4_x86_64.rpm
 vagrant plugin install vagrant-libvirt
 systemctl start libvirtd.service
